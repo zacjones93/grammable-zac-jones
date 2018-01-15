42080e02635d7b011d3165c01c8067d60dcb83a0
Zac Jones <zacjones93@gmail.com>
2018-01-15 12:53:21 -0800

    validates imageso

--- Gemfile
+++ Gemfile
@@ -39,6 +39,10 @@ gem 'simple_form'
 
 gem 'devise'
 
+gem 'carrierwave', '~> 1.0'
+gem "figaro"
+gem "fog-aws"
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -2,7 +2,7 @@ class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-
+    @grams = Gram.all
   end
 
   def new
@@ -59,6 +59,6 @@ class GramsController < ApplicationController
   end
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
 end
